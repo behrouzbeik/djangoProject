1cee49e391096819fcb530156441f6ca3f67fdef
B.Beik <beik.behrouz@gmail.com>
2022-09-06 13:35:03 +0430

    menu items2

--- home/views.py
+++ home/views.py
@@ -7,12 +7,19 @@ def Home(request):
     return redirect('home:Enhome')
 
 def EnHome(request):
+    menu_items = MenuItems.objects.all()
     context = {
         'title': 'QUEEN OF THE TURISM',
         'Lang': 'De',
+        'SignIn':'Sign In',
+        'SignOut':'SignOut',
+        'SignUp':'SignUp',
         'Search': 'Search',
         'Services': 'Services',
-        'Menu': 'Menu'
+        'Menu': 'Menu',
+        'menu_items': menu_items,
+        'Airplane':'Airplane',
+        'Train' : 'Train'
     }
     return render(request, 'home/EnHome.html', context)
 
@@ -21,10 +28,15 @@ def DeHome(request):
     context = {
         'title': 'KÖNIGIN DES TOURISMUS',
         'Lang': 'En',
+        'SignIn': 'Einloggen',
+        'SignOut': 'austragen',
+        'SignUp': 'Anmelden',
         'Search': 'Suche',
         'Services': 'Dienstleistungen',
         'Menu': 'Menü',
-        'menu_items':menu_items
+        'menu_items':menu_items,
+        'Airplane' : 'Flugzeug',
+        'Train' : 'Zug'
     }
     return render(request, 'home/EnHome.html', context)
 
